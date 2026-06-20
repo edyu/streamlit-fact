4063b18ecf41c47052fc632ea939e6e8c3f865d0
Ed Yu <edyu@users.noreply.github.com>
2026-06-20 14:56:18 -0700

    Add state machine to manage flow

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import streamlit as st
 from game import Game
 
-game = Game(st.secrets['llm_api_key'])
+if 'game' not in st.session_state:
+    st.session_state.game = Game(st.secrets['llm_api_key'])
+game = st.session_state.game
 
-question = game.ask_llm_for_question()
-st.container(border=True).write(question)
+if game.status == 'GET_QUESTION':
+    with st.spinner('Obtaining question...') as status:
+        question = game.obtain_question()
+        st.rerun()
 
-answer = st.text_input("Enter your answer")
-if st.button("Submit"):
-    evaluation = game.ask_llm_to_evaluate_answer(question, answer)
-    if evaluation.is_correct:
-        st.success("That's correct")
+elif game.status == 'ASK_QUESTION':
+    st.container(border=True).write(game.curr_question)
+    answer = st.text_input("Enter your answer")
+    if st.button("Submit", type='primary'):
+        game.accept_answer(answer)
+        st.rerun()
+
+elif game.status == 'EVALUATE_ANSWER':
+    with st.spinner('Evaluating answer...') as status:
+        game.evaluate_answer()
+        st.rerun()
+
+elif game.status == 'STATE_RESULT':
+    if game.curr_eval.is_correct:
+        st.success("That's correct!")
     else:
         st.error("Sorry, that's incorrect.")
-        st.info(f"The correct answer was: {evaluation.correct_answer}")
+        st.info(f"The correct answer was: {game.curr_eval.correct_answer}")
